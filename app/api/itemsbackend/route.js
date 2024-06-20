d6b440e7874a905f16c4ab3d15544223386f97ec
meetjadav <meetj7780@gmail.com>
2024-06-20 18:40:04 +0530

    some required changes

---
+++ app/api/itemsbackend/route.js
@@ -0,0 +1,45 @@
+import { NextResponse } from 'next/server';
+import db from '@/utils/db';
+import cookie from 'cookie';
+
+export async function POST(request) {
+    console.log('Received POST request for data insertion');
+    let data = await request.json();
+    const { id, quantity, name, price } = data;
+    const cookies = cookie.parse(request.headers.get('cookie') || '');
+    const userId = cookies.userId;
+
+    // Check if all the necessary fields are provided
+    if (!userId || !id || !quantity || !name || !price) {
+        console.log('Required fields are missing');
+        console.log('Sending 400 response');
+        return NextResponse.json({ message: 'Required fields are missing' }, { status: 400 });
+    }
+
+    // Query to insert the data into the market_data table
+    const query = `
+        INSERT INTO market_data (user_id,item_name, quantity, price)
+        VALUES (?, ?, ?, ?)
+    `;
+    const values = [userId, id, quantity, price];
+
+    try {
+        const results = await new Promise((resolve, reject) => {
+            db.query(query, values, (err, results) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
+
+        console.log('Data inserted successfully');
+        console.log('Sending 200 response');
+        return NextResponse.json({ message: 'Data inserted successfully' }, { status: 200 });
+    } catch (err) {
+        console.error('Error during data insertion:', err);
+        console.log('Sending 500 response');
+        return NextResponse.json({ message: 'Error during data insertion' }, { status: 500 });
+    }
+}
--- app/pages/homepage/marketdynamics/[selectId]/page.js
+++ app/pages/homepage/marketdynamics/[selectId]/page.js
@@ -6,10 +6,12 @@ import NavbarComponent from "@/components/Navbar";
 import marketData from "@/data/marketData"; // Adjust the path based on your project structure
 import "@/app/pages/homepage/marketdynamics/[selectId]/page.css"
 import Image from 'next/image';
-import { getBaseUrl } from '@/utils/getBaseUrl';
 
 const Page = ({ params }) => {
     const [data, setData] = useState(null);
+    const [quantity, setQuantity] = useState(1);
+    const [successMessage, setSuccessMessage] = useState(null);
+    const [failureMessage, setFailureMessage] = useState(null);
     const selectId = params.selectId;
 
     useEffect(() => {
@@ -31,6 +33,33 @@ const Page = ({ params }) => {
         return <div>Loading...</div>;
     }
 
+    const handleBuyNow = async () => {
+        const requestData = {
+            id: selectId,
+            quantity: quantity,
+            name: data.name,
+            price: data.price,
+        };
+
+        const response = await fetch('/api/itemsbackend', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(requestData),
+        });
+
+        if (response.ok) {
+            // Handle successful response
+            console.log('Purchase successful');
+            setSuccessMessage('Purchase successful');
+        } else {
+            // Handle error
+            console.error('Purchase failed');
+            setFailureMessage('Purchase failed!');
+        }
+    };
+
     return (
         <div className="main-cardinfo-container">
             <NavbarComponent />
@@ -39,20 +68,32 @@ const Page = ({ params }) => {
             </div>
             <div className="detail-container">
                 <Image src={data.image} alt={data.name} width={700} height={700} className='detail-image' />
-                <p className="detail-discription">
+                <p className="detail-description">
                     {data.description}
                 </p>
                 <p className="detail-farmersrecomendations">
                     {data.farmerRecommendation}
                 </p>
                 <p className="detail-price">
-                    Price: {data.price}
+                    Price: ${data.price}/Qty
                 </p>
-                <a className="btn-buynow" href={getBaseUrl()}>
+                <div className="quantity-selector">
+                    <label htmlFor="quantity">Quantity</label>
+                    <input
+                        type="number"
+                        id="quantity"
+                        name="quantity"
+                        value={quantity}
+                        min="1"
+                        onChange={(e) => setQuantity(e.target.value)}
+                    />
+                </div>
+                <button className="btn-buynow" onClick={handleBuyNow}>
                     Buy Now
-                </a>
+                </button>
+                {successMessage && <p className="success-message">{successMessage}</p>}
+                {failureMessage && <p className="failure-message">{failureMessage}</p>}
             </div>
-
             <FooterComponent />
         </div>
     );
