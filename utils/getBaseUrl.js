09e92e50bd21498d8c6812ab8e89f514684d7078
meetjadav <meetj7780@gmail.com>
2024-07-15 19:38:36 +0530

    some required changes

--- utils/getBaseUrl.js
+++ utils/getBaseUrl.js
@@ -3,7 +3,7 @@ export const getBaseUrl = () => {
         const { protocol, hostname, port } = window.location;
         return `${protocol}//${hostname}:${port}`;
     } else {
-        const defaultBaseUrl = 'https://krushibazaar.vercel.app';
+        const defaultBaseUrl = 'https://krushibazaar-9gvjggfha-meet-jadavs-projects-4a095c0e.vercel.app';
         return defaultBaseUrl;
     }
 };
