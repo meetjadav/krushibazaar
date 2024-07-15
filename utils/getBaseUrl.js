c97c00b61fbe358302c1449326fddf3e5a5c0e23
meetjadav <meetj7780@gmail.com>
2024-07-15 19:15:04 +0530

    some necessary changes

--- utils/getBaseUrl.js
+++ utils/getBaseUrl.js
@@ -1,7 +1,8 @@
 export const getBaseUrl = () => {
     if (typeof window !== 'undefined') {
         const { protocol, hostname, port } = window.location;
-        return `${protocol}//${hostname}:${port}`;
+        const url = "https://krushibazaar.vercel.app"
+        return `${url}`;
     } else {
         // Fallback for server-side rendering or non-browser environments
         // You might want to customize this depending on your backend environment
