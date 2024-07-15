a1920daad59c54d9ff94d7da738c7c52d4ca3fd5
meetjadav <meetj7780@gmail.com>
2024-07-15 19:16:41 +0530

    some necessary changes

--- utils/getBaseUrl.js
+++ utils/getBaseUrl.js
@@ -3,10 +3,5 @@ export const getBaseUrl = () => {
         const { protocol, hostname, port } = window.location;
         const url = "https://krushibazaar.vercel.app"
         return `${url}`;
-    } else {
-        // Fallback for server-side rendering or non-browser environments
-        // You might want to customize this depending on your backend environment
-        const defaultBaseUrl = 'http://localhost:3000';
-        return defaultBaseUrl;
     }
 };
